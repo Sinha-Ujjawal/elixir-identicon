503f01fedb93586ef001d0fd4aa7d389876c7317
Sinha,Ujjawal <ujjawalsinhacool16021998@gmail.com>
2021-04-11 23:51:12 +0530

    Added logic for conveting string into list of numbers with atleast 15 elements (using md5 hash)

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,18 +1,22 @@
 defmodule Identicon do
-  @moduledoc """
-  Documentation for `Identicon`.
-  """
+  def create(str) do
+    str
+    |> hash_str()
+  end
 
   @doc """
-  Hello world.
+    Converts the given string to a "unique" list of integers
 
-  ## Examples
+    ## Examples
 
-      iex> Identicon.hello()
-      :world
-
+        iex> Identicon.hash_str("apple")
+        [31, 56, 112, 190, 39, 79, 108, 73, 179, 227, 26, 12, 103, 40, 149, 127]
+        iex> Identicon.hash_str("ball")
+        [122, 16, 234, 27, 155, 40, 114, 218, 159, 55, 80, 2, 196, 77, 223, 206]
   """
-  def hello do
-    :world
+  @spec hash_str(str :: charlist()) :: list(integer())
+  def hash_str(str) do
+    :crypto.hash(:md5, str)
+    |> :binary.bin_to_list()
   end
 end
