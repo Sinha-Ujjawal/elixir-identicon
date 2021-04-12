c67243f0d3d54c7c2b5608ca48eeb5f4b2f6aeb5
Sinha,Ujjawal <ujjawalsinhacool16021998@gmail.com>
2021-04-12 20:46:03 +0530

    1. Added structure for data modelling 2. Added logic for picking color from hex field 3. Added logic for making grid from hex field, and storing the grid to the image struct

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -2,6 +2,47 @@ defmodule Identicon do
   def create(str) do
     str
     |> hash_str()
+    |> Identicon.Image.with_hex()
+    |> pick_color()
+    |> make_grid()
+  end
+
+  @doc """
+    Updates `grid` field of `Identicon.Image` struct.
+    Use `hex` field to create the `grid`
+
+    ## Examples
+
+        iex> Identicon.make_grid(%Identicon.Image{hex: [1, 2, 3, 4]})
+        %Identicon.Image{hex: [1, 2, 3, 4], grid: [[1, 2, 3, 2, 1]]}
+        iex> Identicon.make_grid(%Identicon.Image{hex: [1, 2, 3, 4, 5, 6]})
+        %Identicon.Image{hex: [1, 2, 3, 4, 5, 6], grid: [[1, 2, 3, 2, 1], [4, 5, 6, 5, 4]]}
+        iex> Identicon.make_grid(%Identicon.Image{hex: [1, 2, 3, 4, 5, 6, 7, 8]})
+        %Identicon.Image{hex: [1, 2, 3, 4, 5, 6, 7, 8], grid: [[1, 2, 3, 2, 1], [4, 5, 6, 5, 4]]}
+        iex> Identicon.make_grid(%Identicon.Image{hex: [1, 2, 3, 4, 5, 6, 7, 8, 9]})
+        %Identicon.Image{hex: [1, 2, 3, 4, 5, 6, 7, 8, 9], grid: [[1, 2, 3, 2, 1], [4, 5, 6, 5, 4], [7, 8, 9, 8, 7]]}
+  """
+  @spec make_grid(image :: Identicon.Image.t()) :: Identicon.Image.t()
+  def make_grid(image = %Identicon.Image{hex: hex}) do
+    image
+    |> Identicon.Image.with_grid(
+      Enum.chunk_every(hex, 3, 3, :discard)
+      |> Enum.map(fn xs = [x0, x1 | _] -> xs ++ [x1, x0] end)
+    )
+  end
+
+  @doc """
+    Updates `color` field of `Identicon.Image` struct.
+    Uses first 3 values of the `hex` field for `color`
+
+    ## Examples
+
+        iex> Identicon.pick_color(%Identicon.Image{hex: [1, 2, 3, 4]})
+        %Identicon.Image{hex: [1, 2, 3, 4], color: {1, 2, 3}}
+  """
+  @spec pick_color(image :: Identicon.Image.t()) :: Identicon.Image.t()
+  def pick_color(image = %Identicon.Image{hex: [r, g, b | _]}) when length(image.hex) >= 3 do
+    image |> Identicon.Image.with_color({r, g, b})
   end
 
   @doc """
@@ -16,7 +57,6 @@ defmodule Identicon do
   """
   @spec hash_str(str :: charlist()) :: list(integer())
   def hash_str(str) do
-    :crypto.hash(:md5, str)
-    |> :binary.bin_to_list()
+    :crypto.hash(:md5, str) |> :binary.bin_to_list()
   end
 end
---
+++ lib/image.ex
@@ -0,0 +1,83 @@
+defmodule Identicon.Image do
+  defstruct color: nil, hex: nil, grid: nil
+  @type color :: {integer(), integer(), integer()}
+  @type hex :: list(integer())
+  @type grid :: list(list(integer()))
+  @type t :: %Identicon.Image{color: color() | nil, hex: hex() | nil, grid: grid() | nil}
+
+  @doc """
+    Constructor function for struct `Indenticon.Image`
+
+    ## Examples
+
+        iex> Identicon.Image.empty
+        %Identicon.Image{color: nil, hex: nil}
+  """
+  @spec empty() :: t()
+  def empty do
+    %Identicon.Image{}
+  end
+
+  @spec with_hex(hex :: hex()) :: t()
+  def with_hex(hex) do
+    %Identicon.Image{hex: hex}
+  end
+
+  @doc """
+    Constructor function for struct `Indenticon.Image`
+
+    ## Examples
+
+        iex> Identicon.Image.with_hex([1, 2, 3, 4])
+        %Identicon.Image{color: nil, hex: [1, 2, 3, 4]}
+        iex> empty_image = Identicon.Image.empty
+        iex> empty_image |> Identicon.Image.with_hex([1, 2, 3, 4])
+        %Identicon.Image{color: nil, hex: [1, 2, 3, 4]}
+  """
+  @spec with_hex(image :: t(), hex :: hex()) :: t()
+  def with_hex(image, hex) do
+    %Identicon.Image{image | hex: hex}
+  end
+
+  @spec with_color(color :: color()) :: t()
+  def with_color(color) do
+    %Identicon.Image{color: color}
+  end
+
+  @doc """
+    Constructor function for struct `Indenticon.Image`
+
+    ## Examples
+
+        iex> Identicon.Image.with_color({100, 200, 255})
+        %Identicon.Image{color: {100, 200, 255}, hex: nil}
+        iex> empty_image = Identicon.Image.empty
+        iex> empty_image |> Identicon.Image.with_color({100, 200, 255})
+        %Identicon.Image{color: {100, 200, 255}, hex: nil}
+  """
+  @spec with_color(image :: t(), color :: color()) :: t()
+  def with_color(image, color) do
+    %Identicon.Image{image | color: color}
+  end
+
+  @spec with_grid(grid :: grid()) :: t()
+  def with_grid(grid) do
+    %Identicon.Image{grid: grid}
+  end
+
+  @doc """
+    Constructor function for struct `Indenticon.Image`
+
+    ## Examples
+
+        iex> Identicon.Image.with_grid([[1, 2, 3, 4], [5, 6, 7, 8]])
+        %Identicon.Image{grid: [[1, 2, 3, 4], [5, 6, 7, 8]], hex: nil}
+        iex> empty_image = Identicon.Image.empty
+        iex> empty_image |> Identicon.Image.with_grid([[1, 2, 3, 4], [5, 6, 7, 8]])
+        %Identicon.Image{grid: [[1, 2, 3, 4], [5, 6, 7, 8]], hex: nil}
+  """
+  @spec with_grid(image :: t(), grid :: grid()) :: t()
+  def with_grid(image, grid) do
+    %Identicon.Image{image | grid: grid}
+  end
+end
